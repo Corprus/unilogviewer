6a3d6ee09ff74eb5f042d9e057636b188230fb34
Lazer <lazer1999@gmail.com>
2008-04-03 20:20:55 +0000

    [ 1933561 ] What's New File and its execution on installer
    git-svn-id: http://unilogviewer.svn.sourceforge.net/svnroot/unilogviewer@22 9bd6359e-a149-0410-aa77-cebc72bfe646

---
+++ Universal Log Viewer/InstallerClasses/UniLogViewerInstaller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+
+
+namespace InstallerClasses
+{
+    [RunInstaller(true)]
+    public partial class WhatsNew : Installer
+    {
+    
+        public WhatsNew()
+        {
+            InitializeComponent();
+        }
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+            System.Diagnostics.Process batch = new System.Diagnostics.Process();
+            batch.StartInfo.FileName = "notepad.exe";
+            string InstallPath = this.Context.Parameters["assemblypath"];
+            InstallPath = InstallPath.Substring(0, InstallPath.LastIndexOf("\\"));
+            batch.StartInfo.WorkingDirectory = this.Context.Parameters["assemblypath"];
+            batch.StartInfo.Arguments = "\"" + InstallPath + "\\Docs\\WhatsNew.txt\"";
+            batch.Start();
+            
+        }
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}
