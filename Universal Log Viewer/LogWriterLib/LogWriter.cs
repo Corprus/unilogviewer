aca2a79f25b572e1cd9f8e3a5b28c716ef23cc9e
War-Deda <war.npu3pak@gmail.com>
2008-04-08 06:35:59 +0000

    [ 1934141 ] Logging DLL
    git-svn-id: http://unilogviewer.svn.sourceforge.net/svnroot/unilogviewer@34 9bd6359e-a149-0410-aa77-cebc72bfe646

---
+++ Universal Log Viewer/LogWriterLib/LogWriter.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LogWriterLib
+{
+    
+    public class LogWriter
+    {
+        TextWriter _oTextWriter;
+
+        public enum TypeLogMessage { LMT_ERROR=0, LMT_FATAL=1, LMT_WARN=2, LMT_INFORM=3 };
+        private String[] aTypeMessages = { "ERROR", "FATAL", "WARN", "INFORM" };
+
+        public LogWriter(String vsFileName)
+        {
+            _oTextWriter = new StreamWriter(vsFileName, true);
+        }
+
+        ~LogWriter()
+        {
+            
+        }
+
+        public int WriteLog(TypeLogMessage ventType, String vsMessage)
+        {
+            string sMessage = "[" + Convert.ToString(DateTime.Now) + "]\t";
+            sMessage += "[" + aTypeMessages[(int)ventType] + "]\t" + vsMessage;
+            _oTextWriter.WriteLine((string)sMessage);
+            _oTextWriter.Flush();
+            return 0;
+        }
+
+    }
+}
