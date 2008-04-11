31fb2f67135cea88c814115ac0b79249c98cb268
Lazer <lazer1999@gmail.com>
2008-04-11 06:45:27 +0000

    New minor version 0.2.2
    git-svn-id: http://unilogviewer.svn.sourceforge.net/svnroot/unilogviewer@46 9bd6359e-a149-0410-aa77-cebc72bfe646

--- Universal Log Viewer/Universal Log Viewer/Properties/AssemblyInfo.cs
+++ Universal Log Viewer/Universal Log Viewer/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.1.0")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.2.2.0")]
+[assembly: AssemblyFileVersion("0.2.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
