e0761797ff09ddc85362b5674c8a83768e826fb1
Lazer <lazer1999@gmail.com>
2008-05-19 10:36:24 +0000

    Beta (0.3.0.0) release
    git-svn-id: http://unilogviewer.svn.sourceforge.net/svnroot/unilogviewer@69 9bd6359e-a149-0410-aa77-cebc72bfe646

--- Universal Log Viewer/Universal Log Viewer/Properties/AssemblyInfo.cs
+++ Universal Log Viewer/Universal Log Viewer/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.5.0")]
-[assembly: AssemblyFileVersion("0.2.5.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
